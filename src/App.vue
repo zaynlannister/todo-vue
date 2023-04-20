0888b680ba8fc52c9a038af5ee9f7495aa803285
zaynlannister <zaynlannister@gmail.com>
2023-04-20 11:31:42 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="app">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+      sequi.
+    </p>
+  </div>
+</template>
+
+<style>
+.app {
+  width: 1120px;
+  margin: 0 auto;
+}
+</style>
